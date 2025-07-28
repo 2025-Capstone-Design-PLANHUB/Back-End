78d9d08766daafef19bf92da95041698fafa415b
jjh75607 <dkrak5738@naver.com>
2025-07-28 17:34:35 +0900

    [#13] Feat: Oauth2GithubService 구현

--- src/main/java/soon/planhub/domain/member/repository/MemberJpaRepository.java
+++ src/main/java/soon/planhub/domain/member/repository/MemberJpaRepository.java
@@ -1,8 +1,11 @@
 package soon.planhub.domain.member.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import soon.planhub.domain.member.entity.Member;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByNickname(String nickname);
 
 }
--- src/main/java/soon/planhub/domain/member/repository/MemberRepository.java
+++ src/main/java/soon/planhub/domain/member/repository/MemberRepository.java
@@ -1,12 +1,25 @@
 package soon.planhub.domain.member.repository;
 
+import static soon.planhub.global.exception.dto.ErrorDetail.MEMBER_NOT_FOUND;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import soon.planhub.domain.member.entity.Member;
+import soon.planhub.global.exception.dto.common.EntityNotFoundException;
 
 @RequiredArgsConstructor
 @Repository
 public class MemberRepository {
 
     private final MemberJpaRepository memberJpaRepository;
+
+    public Member findByNickname(String nickname) {
+        return memberJpaRepository.findByNickname(nickname)
+            .orElseThrow(() -> new EntityNotFoundException(MEMBER_NOT_FOUND));
+    }
+
+    public void save(Member member) {
+        memberJpaRepository.save(member);
+    }
 
 }
