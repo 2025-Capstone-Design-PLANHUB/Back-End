00a9cdcd758ffce35e242024b5d851afb459a02b
jjh75607 <dkrak5738@naver.com>
2025-08-08 20:45:03 +0900

    [#25] Feat: 초대 코드 전송 API 구현 - sendInvitationEmails 구현 - request dto 구현 - 테스트코드 작성

--- src/main/java/soon/planhub/domain/team/controller/InvitationController.java
+++ src/main/java/soon/planhub/domain/team/controller/InvitationController.java
@@ -1,22 +1,21 @@
 package soon.planhub.domain.team.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import soon.planhub.domain.team.controller.dto.request.InvitationSendRequest;
 import soon.planhub.domain.team.service.invitation.InvitationService;
 import soon.planhub.global.annotation.AuthMemberId;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/teams/{teamId}/invitation-codes")
+@RequestMapping("/api/v1/teams/{teamId}")
 @RestController
 public class InvitationController {
 
     private final InvitationService invitationService;
 
-    @PostMapping
+    @PostMapping("/invitation-codes")
     public ResponseEntity<String> generateInvitationCode(
         @AuthMemberId Long memberId,
         @PathVariable Long teamId
@@ -24,6 +23,17 @@ public class InvitationController {
         String invitationCode = invitationService.generateInvitationCode(teamId, memberId);
 
         return ResponseEntity.ok(invitationCode);
+    }
+
+    @PostMapping("/invitations")
+    public ResponseEntity<Void> sendInvitationCode(
+        @Valid @RequestBody InvitationSendRequest request,
+        @AuthMemberId Long memberId,
+        @PathVariable Long teamId
+    ) {
+        invitationService.sendInvitationCode(request.toServiceRequest(teamId, memberId));
+
+        return ResponseEntity.noContent().build();
     }
 
 }
