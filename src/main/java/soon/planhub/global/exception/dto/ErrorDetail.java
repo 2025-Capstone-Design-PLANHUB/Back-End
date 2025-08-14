cd5eb27e83f65d15973e27171a92721ecb1af460
jjh75607 <dkrak5738@naver.com>
2025-08-14 14:53:56 +0900

    [#29] Feat: Github org 멤버 추가 기능 추가 - GithubOrganizationAppender 추가 - OauthTokenExpiredException 예외 추가

--- src/main/java/soon/planhub/global/exception/dto/ErrorDetail.java
+++ src/main/java/soon/planhub/global/exception/dto/ErrorDetail.java
@@ -12,6 +12,7 @@ public enum ErrorDetail {
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
     UNAUTHORIZED(401, "인증되지 않은 사용자입니다."),
     EMAIL_SEND_FAILED(500, "이메일 전송에 실패했습니다."),
+    OAUTH_TOKEN_EXPIRED(401, "OAuth 토큰이 만료되었습니다."),
 
     // 회원 관련
     MEMBER_NOT_FOUND(404, "해당 회원을 찾을 수 없습니다."),
---
+++ src/main/java/soon/planhub/infra/github/organization/GithubOrganizationAppender.java
@@ -0,0 +1,39 @@
+package soon.planhub.infra.github.organization;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
+import soon.planhub.global.exception.dto.ErrorDetail;
+import soon.planhub.global.exception.member.OAuthTokenExpiredException;
+import soon.planhub.infra.client.RestClientProvider;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class GithubOrganizationAppender {
+
+    private static final String ORGANIZATION_MEMBER_URL = "/orgs/{organizationName}/memberships/{username}";
+
+    private final RestClientProvider restClientProvider;
+
+    public void appendMemberToOrg(String orgName, String nickname, String oauthToken) {
+        RestClient client = restClientProvider.createClient(oauthToken);
+
+        try {
+            client.put()
+                .uri(ORGANIZATION_MEMBER_URL, orgName, nickname)
+                .retrieve()
+                .body(Void.class);
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode().value() == UNAUTHORIZED.value()) {
+                throw new OAuthTokenExpiredException(ErrorDetail.OAUTH_TOKEN_EXPIRED);
+            }
+            log.error("organization 멤버 추가 중 에러 발생 member: {}, organization: {}", nickname, orgName, e);
+        }
+    }
+
+}
