c92c13b562a3db56744c227e337e1e323f468ff4
jjh75607 <dkrak5738@naver.com>
2025-08-14 16:20:29 +0900

    [#29] Feat: 팀 멤버 추가 API 구현 - TeamController 엔드포인트 추가 - 테스트 구현

--- src/main/java/soon/planhub/domain/team/controller/TeamController.java
+++ src/main/java/soon/planhub/domain/team/controller/TeamController.java
@@ -2,12 +2,11 @@ package soon.planhub.domain.team.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import soon.planhub.domain.team.controller.dto.request.TeamCreateRequest;
+import soon.planhub.domain.team.controller.dto.request.TeamJoinRequest;
 import soon.planhub.domain.team.service.TeamService;
 import soon.planhub.global.annotation.AuthMemberId;
 
@@ -25,6 +24,17 @@ public class TeamController {
     ) {
         Long teamId = teamService.createTeam(request.toServiceRequest(), memberId);
         return ResponseEntity.ok(teamId);
+    }
+
+    @PostMapping("/{teamId}/members")
+    public ResponseEntity<Long> joinTeam(
+        @Valid @RequestBody TeamJoinRequest request,
+        @AuthMemberId Long memberId,
+        @PathVariable Long teamId
+    ) {
+        Long joinedTeamId = teamService.joinTeam(request.toServiceRequest(teamId), memberId);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(joinedTeamId);
     }
 
 }
