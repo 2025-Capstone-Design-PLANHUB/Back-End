04ab9e356668b4e16e15aa4e8789c288fa30e896
jjh75607 <dkrak5738@naver.com>
2025-08-17 17:48:25 +0900

    [#33] Feat: 팀 멤버 목록 조회 API 및 테스트 추가

--- src/main/java/soon/planhub/domain/teammember/controller/TeamMemberController.java
+++ src/main/java/soon/planhub/domain/teammember/controller/TeamMemberController.java
@@ -7,7 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import soon.planhub.domain.teammember.controller.dto.request.TeamMemberAppendRequest;
 import soon.planhub.domain.teammember.service.TeamMemberService;
+import soon.planhub.domain.teammember.service.dto.response.TeamMemberDetailResponse;
 import soon.planhub.global.annotation.AuthMemberId;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/teams/{teamId}/members")
@@ -25,6 +28,15 @@ public class TeamMemberController {
         Long joinedTeamId = teamMemberService.append(request.toServiceRequest(teamId), memberId);
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(joinedTeamId);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TeamMemberDetailResponse>> getTeamMembers(
+        @AuthMemberId Long memberId,
+        @PathVariable Long teamId
+    ) {
+        List<TeamMemberDetailResponse> response = teamMemberService.getTeamMembers(teamId, memberId);
+        return ResponseEntity.ok(response);
     }
 
 }
