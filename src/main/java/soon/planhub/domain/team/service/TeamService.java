ca0d48c9cd4c55b8126e76468d822b745cd3a66b
jjh75607 <dkrak5738@naver.com>
2025-08-17 16:09:39 +0900

    [#31] Refactor: 팀 참여 관련 서비스 및 테스트 코드 제거

--- src/main/java/soon/planhub/domain/team/service/TeamService.java
+++ src/main/java/soon/planhub/domain/team/service/TeamService.java
@@ -3,32 +3,17 @@ package soon.planhub.domain.team.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import soon.planhub.domain.team.service.dto.request.TeamCreateServiceRequest;
-import soon.planhub.domain.team.service.dto.request.TeamJoinServiceRequest;
-import soon.planhub.domain.teammember.port.out.TeamMemberPort;
-import soon.planhub.domain.teammember.service.TeamMemberAppender;
-
-import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Service
 public class TeamService {
 
     private final TeamCreator teamCreator;
-    private final TeamMemberAppender teamMemberAppender;
     private final TeamValidator teamValidator;
-    private final TeamMemberPort teamMemberPort;
 
     public Long createTeam(TeamCreateServiceRequest request, Long creatorId) {
         teamValidator.validateAdminPermission(request.organizationName(), creatorId);
         return teamCreator.createTeam(request.toInfo(), creatorId);
-    }
-
-    public Long joinTeam(TeamJoinServiceRequest request, Long memberId) {
-        LocalDateTime now = LocalDateTime.now();
-        teamValidator.validateInvitationCode(request.invitationCode(), now);
-
-        teamMemberPort.appendMemberToOrg(request.teamId(), memberId); // 깃 허브에 요청
-        return teamMemberAppender.appendToMember(memberId, request.teamId(), request.position());
     }
 
 }
