30e476be25de2fa7d59fe5ae28c3526b96d46aa1
jjh75607 <dkrak5738@naver.com>
2025-08-22 18:04:22 +0900

    [#41] Feat: 태스크 템플릿 수정 로직 및 테스트 추가

--- src/main/java/soon/planhub/domain/task/service/template/TaskTemplateProcessor.java
+++ src/main/java/soon/planhub/domain/task/service/template/TaskTemplateProcessor.java
@@ -2,12 +2,14 @@ package soon.planhub.domain.task.service.template;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import soon.planhub.domain.project.repository.ProjectRepository;
 import soon.planhub.domain.task.entity.TaskTemplate;
 import soon.planhub.domain.task.repository.TaskTemplateRepository;
 import soon.planhub.domain.task.service.dto.template.TaskTemplateInformation;
 
 @RequiredArgsConstructor
+@Transactional
 @Component
 public class TaskTemplateProcessor {
 
@@ -24,6 +26,16 @@ public class TaskTemplateProcessor {
         );
         taskTemplateRepository.save(taskTemplate);
         return taskTemplate.getId();
+    }
+
+    public void updateTaskTemplate(TaskTemplateInformation info, Long taskTemplateId) {
+        TaskTemplate template = taskTemplateRepository.findById(taskTemplateId);
+        template.update(
+            info.title(),
+            info.description(),
+            info.content(),
+            info.type()
+        );
     }
 
 }
