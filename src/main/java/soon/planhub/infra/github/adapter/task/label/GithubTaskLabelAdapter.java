91d1893ed3d0f42e2de2fbbf82cb66e55be2892d
jjh75607 <dkrak5738@naver.com>
2025-08-22 22:07:00 +0900

    [#49] Feat: GitHub 이슈 라벨 수정 port, adapter 추가

--- src/main/java/soon/planhub/infra/github/adapter/task/label/GithubTaskLabelAdapter.java
+++ src/main/java/soon/planhub/infra/github/adapter/task/label/GithubTaskLabelAdapter.java
@@ -7,8 +7,11 @@ import soon.planhub.domain.project.entity.Project;
 import soon.planhub.domain.project.repository.ProjectRepository;
 import soon.planhub.domain.task.port.out.TaskLabelPort;
 import soon.planhub.domain.task.service.dto.label.TaskLabelInformation;
+import soon.planhub.domain.task.service.dto.label.request.TaskLabelUpdateServiceRequest;
 import soon.planhub.infra.github.dto.GithubIssueLabelCreateRequest;
+import soon.planhub.infra.github.dto.GithubIssueLabelUpdateRequest;
 import soon.planhub.infra.github.task.label.GithubIssueLabelCreator;
+import soon.planhub.infra.github.task.label.GithubIssueLabelModifier;
 
 @RequiredArgsConstructor
 @Component
@@ -17,6 +20,7 @@ public class GithubTaskLabelAdapter implements TaskLabelPort {
     private final MemberRepository memberRepository;
     private final ProjectRepository projectRepository;
     private final GithubIssueLabelCreator githubIssueLabelCreator;
+    private final GithubIssueLabelModifier githubIssueLabelModifier;
 
     @Override
     public void createLabel(TaskLabelInformation info, Long memberId, Long projectId) {
@@ -24,6 +28,14 @@ public class GithubTaskLabelAdapter implements TaskLabelPort {
         Project project = projectRepository.findById(projectId);
 
         githubIssueLabelCreator.createGithubIssueLabelAsync(GithubIssueLabelCreateRequest.from(oauthToken, info, project));
+    }
+
+    @Override
+    public void updateLabel(TaskLabelUpdateServiceRequest request, Long memberId) {
+        String oauthToken = memberRepository.findById(memberId).getOauthToken();
+        Project project = projectRepository.findById(request.projectId());
+
+        githubIssueLabelModifier.updateGithubIssueLabelAsync(GithubIssueLabelUpdateRequest.from(oauthToken, project, request));
     }
 
 }
