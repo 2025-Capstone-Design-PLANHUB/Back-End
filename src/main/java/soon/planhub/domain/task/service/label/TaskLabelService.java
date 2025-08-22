cfb0f8464ba311473a80e4892a3f6ea339182add
jjh75607 <dkrak5738@naver.com>
2025-08-22 22:07:27 +0900

    [#49] Feat: 태스크 라벨 수정 기능 및 테스트 추가

--- src/main/java/soon/planhub/domain/task/service/label/TaskLabelService.java
+++ src/main/java/soon/planhub/domain/task/service/label/TaskLabelService.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import soon.planhub.domain.task.port.out.TaskLabelPort;
 import soon.planhub.domain.task.service.dto.label.request.TaskLabelCreateServiceRequest;
+import soon.planhub.domain.task.service.dto.label.request.TaskLabelUpdateServiceRequest;
 
 @RequiredArgsConstructor
 @Service
 public class TaskLabelService {
 
     private final TaskLabelCreator taskLabelCreator;
+    private final TaskLabelModifier taskLabelModifier;
     private final TaskLabelValidator taskLabelValidator;
     private final TaskLabelPort taskLabelPort;
 
@@ -18,6 +20,15 @@ public class TaskLabelService {
 
         taskLabelPort.createLabel(request.toInfo(), memberId, request.projectId()); // github API 호출
         return taskLabelCreator.createLabel(request.toInfo(), request.projectId());
+    }
+
+    public void updateLabel(TaskLabelUpdateServiceRequest request, Long memberId) {
+        if (!request.oldTitle().equals(request.newTitle())) {
+            taskLabelValidator.validateLabelNotExists(request.newTitle(), request.projectId());
+        }
+
+        taskLabelModifier.updateLabel(request.toInfo(), request.labelId());
+        taskLabelPort.updateLabel(request, memberId); // github API 호출
     }
 
 }
