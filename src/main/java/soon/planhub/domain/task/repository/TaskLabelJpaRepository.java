bdcc09591cdb14f9b26cfec9ba6692f23d5eb923
jjh75607 <dkrak5738@naver.com>
2025-08-24 18:48:06 +0900

    [#55] Feat: 프로젝트별 라벨 목록 조회 및 라벨 일괄 저장 기능 추가

--- src/main/java/soon/planhub/domain/task/repository/TaskLabelJpaRepository.java
+++ src/main/java/soon/planhub/domain/task/repository/TaskLabelJpaRepository.java
@@ -3,8 +3,12 @@ package soon.planhub.domain.task.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import soon.planhub.domain.task.entity.TaskLabel;
 
+import java.util.List;
+
 public interface TaskLabelJpaRepository extends JpaRepository<TaskLabel, Long> {
 
     boolean existsByTitleAndProjectId(String title, Long projectId);
+
+    List<TaskLabel> findAllByProjectId(Long projectId);
 
 }
